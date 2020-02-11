1eba70206fb772cec2c9171d82ccb6ede584f904
Emily Marigold Klassen <forivall@gmail.com>
2020-02-11 14:02:13 -0800

    fixup! Fix type signature of takeInto

--- take-into.d.ts
+++ take-into.d.ts
@@ -2,4 +2,4 @@ import {default as ObliteratorIterator} from './iterator.js';
 
 // Requires a resolution of https://github.com/microsoft/TypeScript/issues/1213
 // export default function takeInto<C<~>, T>(ArrayClass: new <T>(n: number) => C<T>, iterator: Iterator<T>, n: number): C<T>;
-export default takeInto<T>(ArrayClass: new <T>(arrayLength: number) => T[], iterator: Iterator<T>, n: number): T[];
+export default function takeInto<T>(ArrayClass: new <T>(arrayLength: number) => T[], iterator: Iterator<T>, n: number): T[];
